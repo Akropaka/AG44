f37f5ef8b3cafa167be36b8c8cdf76ad4c20ff00
telhajhu <el-haj-hussein.thomas@live.fr>
2020-01-14 16:35:50 +0100

    Image runtime

--- TP_AG44/main.cpp
+++ TP_AG44/main.cpp
@@ -7,16 +7,16 @@ using namespace std;
 
 int main()
 {
-    graph g(5,true);
+    graph g(10,true);
     //g.toFileAdjList("AdjList");
-    //g.toFileMatrix("Matrix");
+    g.toFileMatrix("Matrix");
     g.getGraphFromFile("Matrix");
     g.printMatrix();
-    //g.printAdjToVertex(2);
-    /*printVectorInt(g.BFS(0));
+    g.printAdjToVertex(2);
+    printVectorInt(g.BFS(0));
     cout << endl;
     printVectorInt(g.DFS(0));
-    cout << endl;*/
-    g.showStrongly_Related();
+    cout << endl;
+    //g.showStrongly_Related();
     return 0;
 }
